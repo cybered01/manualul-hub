0fb30e9928eb22e08eeef482ff4ec3c90e567b51
Motanul <86203111+cybered01@users.noreply.github.com>
2026-01-23 21:36:20 +0200

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // IMPORTANT: Google are nevoie de acest URL pentru a genera corect harta site-ului
-  // După ce cumperi domeniul .ro, vei schimba aici cu https://manualul.ro
   site: 'https://manualul.ro',
 
   vite: {
